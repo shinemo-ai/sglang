58af55cb2b51036d4e1711f2f370419e3d6c71d9
Legolas <frank747300711@126.com>
2026-05-30 22:33:54 +0800

    Revert "init commit"
    Incorrect branch commit
    
    This reverts commit 323e8cd04e2fd9856ca9b904d388109844792968.

--- python/sglang/srt/managers/tokenizer_manager.py
+++ python/sglang/srt/managers/tokenizer_manager.py
@@ -2141,18 +2141,14 @@ class TokenizerManager(TokenizerControlMixin, TokenizerManagerScoreMixin):
         # We should batch all top-k tokens in all positions.
         ret = []
         for i in range(len(token_logprobs_val)):
-            val, idx = token_logprobs_val[i], token_logprobs_idx[i]
-            if isinstance(val, torch.Tensor):
-                if val.numel() == 0:
-                    ret.append(None)
-                    continue
-                val = val.tolist()
-            elif not val:
+            if token_logprobs_val[i]:
+                ret.append(
+                    self.detokenize_logprob_tokens(
+                        token_logprobs_val[i], token_logprobs_idx[i], decode_to_text
+                    )
+                )
+            else:
                 ret.append(None)
-                continue
-            if isinstance(idx, torch.Tensor):
-                idx = idx.tolist()
-            ret.append(self.detokenize_logprob_tokens(val, idx, decode_to_text))
         return ret
 
     def _calculate_spec_decoding_metrics(
