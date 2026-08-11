719c24acac59bba3b3079883996a3380524f4727
Legolas <frank747300711@126.com>
2026-08-11 14:18:08 +0800

    update

--- python/sglang/srt/arg_groups/deepseek_v4_hook.py
+++ python/sglang/srt/arg_groups/deepseek_v4_hook.py
@@ -169,6 +169,7 @@ def validate_deepseek_v4_cp(server_args: ServerArgs) -> None:
     server_args.enable_dsa_prefill_context_parallel = True
     server_args.dsa_prefill_cp_mode = "round-robin-split"
     server_args.enable_dp_attention = True
+    server_args.enable_dp_lm_head = True
     server_args.moe_dense_tp_size = 1
     server_args.attn_cp_size = server_args.tp_size // server_args.dp_size
     assert (
